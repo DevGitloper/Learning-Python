a01dcd63846184ca4bfb33adbece87bd1d4925a1
- <devgitloperhub@gmail.com>
2022-01-09 00:42:47 +0100

    Create main.py

---
+++ boji/main.py
@@ -0,0 +1,13 @@
+from time import sleep
+from typing import ParamSpec
+
+vupros1 = input("pro li si na fortnite? ")
+
+if vupros1 == str("da"):
+    slee(2)
+
+    print("okeh crankni edno nainti tokava!")
+
+
+elif str("ne"):
+    print("gei")
