788e0f3d9fd9a23d48a6570cbf50c9c289a3e817
- <devgitloperhub@gmail.com>
2022-01-09 01:01:30 +0100

    main

--- toni/main.py
+++ toni/main.py
@@ -1,13 +1,1 @@
-from time import sleep
-from typing import ParamSpec
-
-vupros1 = input("pro li si na fortnite? ")
-
-if vupros1 == str("da"):
-    slee(2)
-
-    print("okeh crankni edno nainti tokava!")
-
-
-elif str("ne"):
-    print("gei")
+Toni se uchi na python i e mnogo burzo zagrqvasht pri tegleneto!!!
